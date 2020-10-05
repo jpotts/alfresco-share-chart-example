cae5ffb7bb9541193b9c6a5e4bdfc62b5d77f6b8
Jeff Potts <jeffpotts01@gmail.com>
2020-10-05 14:39:43 -0500

    Add an Aikau dashlet that will eventually hold the chart.

---
+++ alfresco-share-chart-example-share/src/main/resources/META-INF/resources/alfresco-share-chart-example-share/js/examples/widgets/CustomReportDashlet.js
@@ -0,0 +1,40 @@
+/**
+ * CustomReportDashlet copied from ContentReportDashlet by Erik Winlöf
+ *
+ * @module examples/widgets/CustomReportDashlet
+ * @extends alfresco/dashlet/Dashlet
+ * @author Jeff Potts
+ */
+define(["dojo/_base/declare",
+  "alfresco/core/Core",
+  "alfresco/core/I18nUtils",
+  "alfresco/dashlets/Dashlet"],
+     function(declare, AlfCore, I18nUtils, Dashlet) {
+
+        return declare([Dashlet], {
+
+           /**
+            * The i18n scope to use for this widget.
+            *
+            * @instance
+            */
+           i18nScope: "examples.widgets.CustomReportDashlet",
+
+           /**
+            * An array of the i18n files to use with this widget.
+            *
+            * @instance
+            * @type {object[]}
+            * @default [{i18nFile: "./i18n/CustomReportDashlet.properties"}]
+            */
+           i18nRequirements: [{i18nFile: "./i18n/CustomReportDashlet.properties"}],
+
+           /**
+            * The widgets to be processed to generate each item in the rendered view.
+            *
+            * @instance
+            * @type {object[]}
+            */
+            widgetsForBody: []
+        });
+     });
---
+++ alfresco-share-chart-example-share/src/main/resources/alfresco/web-extension/site-webscripts/com/ecmarchitect/dashlets/sales-summary-chart.get.js
@@ -0,0 +1,12 @@
+model.jsonModel = {
+   rootNodeId: args.htmlid,
+   pubSubScope: instance.object.id,
+   services: [
+   ],
+   widgets:[
+      {
+        id: "DASHLET",
+        name: "examples/widgets/CustomReportDashlet"
+        }
+    ]
+};
