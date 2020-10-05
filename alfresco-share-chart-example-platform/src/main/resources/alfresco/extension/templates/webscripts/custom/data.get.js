02518663d8f3f49a10dde5d4f4e404bfa5e7edf2
Jeff Potts <jeffpotts01@gmail.com>
2020-10-05 13:27:20 -0500

    Add repo tier web script that returns data we want to chart

---
+++ alfresco-share-chart-example-platform/src/main/resources/alfresco/extension/templates/webscripts/custom/data.get.js
@@ -0,0 +1,9 @@
+var valuesList = [];
+
+// This data could come from anywhere--Alfresco or some other API
+valuesList.push({region: "West", total: 125});
+valuesList.push({region: "North", total: 75});
+valuesList.push({region: "East", total: 250});
+valuesList.push({region: "South", total: 150});
+
+model.valuesList = valuesList;
